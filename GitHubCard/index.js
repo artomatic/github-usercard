3449a1f5d41c96956519c73a3f244014dff4f8f5
Art Vardanyan <akvardanyan99@gmail.com>
2023-07-22 23:22:10 -0400

    submission

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,19 @@
+import axios from 'axios';
+
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get('https://api.github.com/users/artomatic')
+  .then (resp => {
+    console.log(resp)
+    cardMaker(resp)
+  })
+  .catch (err => console.error(err))
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +39,13 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+];
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -50,6 +67,59 @@ const followersArray = [];
     </div>
 */
 
+function cardMaker (obj) {
+
+  const cardWrapper = document.createElement('div')
+  const image = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const profileURL = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+
+  cardWrapper.classList.add('card')
+  image.setAttribute('src', obj.data.avatar_url)
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  name.textContent = obj.data.name
+  username.classList.add('username')
+  username.textContent = obj.data.login
+  location.textContent = 'Location: ' + obj.data.location
+  profile.textContent = 'Profile: ' + obj.data.html_url
+  profileURL.href = obj.data.html_url
+  followers.textContent = 'Followers: ' + obj.data.followers
+  following.textContent = 'Following: ' + obj.data.following
+  bio.textContent = 'Bio: ' + obj.data.bio
+
+
+  document.querySelector('body').appendChild(cardWrapper)
+  cardWrapper.appendChild(image)
+  cardWrapper.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  cardInfo.appendChild(profileURL)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+}
+
+followersArray.forEach( person => {
+  axios.get(`https://api.github.com/users/${person}`)
+  .then (resp => {
+    console.log(resp)
+    cardMaker(resp)
+  })
+  .catch (err => console.error(err))
+})
+
 /*
   List of LS Instructors Github username's:
     tetondan
@@ -58,3 +128,5 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
+
